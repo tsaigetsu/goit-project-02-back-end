6b48e810961d7a486819a25a56bcc97a78709fc3
Denny <d3n1sf4z49@gmail.com>
2024-11-16 14:53:02 +0100

    update max letters discription validate

--- src/constants/users.js
+++ src/constants/users.js
@@ -1,4 +1,2 @@
 export const emailRegexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 // export const passwordRegexp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/;
-
-console.log('Email matches:', emailRegexp.test('yomi222e@anypng.com'));
