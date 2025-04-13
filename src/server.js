3e96c96293432e5ae5acb222e8909d0d228f4e7c
Denny <d3n1sf4z49@gmail.com>
2025-04-14 01:30:58 +0200

    added credetials

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ import { swaggerDocs } from './middlewares/swaggerDocs.js';
 const PORT = Number(env(ENV_VARS.PORT, 3000));
 
 const corsOptions = {
-  origin: '*',
+  origin: 'https://goit-project-02-front-end-smoky.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
@@ -24,6 +24,23 @@ export const setupServer = () => {
 
   app.use(cors(corsOptions));
   app.use(cookieParser());
+
+  app.use((req, res, next) => {
+    res.header(
+      'Access-Control-Allow-Origin',
+      'https://goit-project-02-front-end-smoky.vercel.app',
+    );
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+    );
+    res.header(
+      'Access-Control-Allow-Methods',
+      'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+    );
+    next();
+  });
 
   app.use(
     pino({
